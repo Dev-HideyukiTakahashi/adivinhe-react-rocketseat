140f2c33ea1956f7d9294be7c098b7a3931d3324
Hideyuki <dev.hideyukitakahashi@gmail.com>
2025-07-16 01:30:00 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## App Adivinhe
+
+---
+
+Para iniciar uma aplicação React com Vite, utilize o comando:
+
+```bash
+npm create vite@latest
+```
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello World</h1>
+    <button>teste</button>
+    </>
+  )
+}
+
+export default App
