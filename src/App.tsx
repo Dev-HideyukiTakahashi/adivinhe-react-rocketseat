cce440456f53210eb08bbedfba0ec079c8d6eb6d
Hideyuki <dev.hideyukitakahashi@gmail.com>
2025-07-16 02:58:46 -0300

    interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import styles from './app.module.css';
+import { Button } from './components/button';
 import { Header } from './components/header';
+import { Input } from './components/input';
+import { Letter } from './components/letter';
+import { LettersUsed } from './components/letters-used';
+import { Tip } from './components/tip';
+
 
 function handleRestartGame() {
-  alert("Reiniciar o jogo")
+  alert('Reiniciar o jogo');
 }
 
 function App() {
@@ -10,6 +16,23 @@ function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Linguagem de programação dinâmica" />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder='?'/>
+          <Button title="Confirmar"/>
+        </div>
+
+        <LettersUsed/>
       </main>
     </div>
   );
---
+++ src/components/letters-used/index.tsx
@@ -0,0 +1,16 @@
+import { Letter } from '../letter'
+import styles from './styles.module.css'
+
+export function LettersUsed() {
+  
+  return(
+    <div className={styles.lettersUsed}>
+      <h5>Letras utilizadas</h5>
+      <div>
+
+      <Letter value='R'  size='small' color='wrong'/>
+      <Letter value='X'  size='small' />
+      </div>
+    </div>
+  )
+}
